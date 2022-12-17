1369f267c210807a0117f4699f2c322291b35251
trofchik <trofimov.nikita20@gmail.com>
2022-12-17 04:36:11 +0400

    Added flags for passing in query and printing result's attributes.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,25 +11,45 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-var query = "Ремонт"
-
-func init() {} // parse flags for query
+func init() {
+	log.SetFlags(log.Flags() &^ (log.Ltime | log.Ldate))
+}
 
 func main() {
+	// doesn' work properly if placed inside init() for some reason
+
+	query := flag.String("q", "поставка", "Query to search orders with. No spaces allowed yet.")
+	withUID := flag.Bool("uid", false, "Set to print uids of orders.")
+	withType := flag.Bool("type", false, "Set to print types of orders.")
+	withTitle := flag.Bool("title", false, "Set to print titles of orders.")
+	withTimestamp := flag.Bool("timestamp", false, "Set to print publication timestamps of orders.")
+	flag.Parse()
+
+	fmt.Println(*query)
+	fmt.Println(*withUID)
+	fmt.Println(*withType)
+	fmt.Println(*withTitle)
+	fmt.Println(*withTimestamp)
 
 	// vary values for page, on_page, query etc.
 	url := fmt.Sprintf(
 		"https://www.fabrikant.ru/trades/procedure/search/?type=0&procedure_stage=0&currency=0&date_type=date_publication&ensure=all&count_on_page=40&order_direction=1&query=%s&page=1",
-		query,
+		*query,
 	)
 	req, _ := http.NewRequest("GET", url, nil)
-	// req.AddCookie(resp.Cookies()[0])
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
 
 	resp, err := (&http.Client{}).Do(req)
 	if err != nil {
 		log.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		// s := bufio.NewScanner(resp.Body)
+		// for s.Scan() {
+		// 	fmt.Println(s.Text())
+		// }
+		log.Fatal("Error making http request: ", resp.StatusCode)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
@@ -44,7 +65,41 @@ func main() {
 			orders = append(orders, fabrikant.NewOrderFromInnerGridSelection(s))
 		})
 
+	printUid := func(o *fabrikant.Order) {
+		fmt.Print(o.UID, " | ")
+	}
+	printType := func(o *fabrikant.Order) {
+		fmt.Print(o.Type, " | ")
+	}
+	printTitle := func(o *fabrikant.Order) {
+		fmt.Print(o.Title, " | ")
+	}
+	printTimestamp := func(o *fabrikant.Order) {
+		fmt.Print(o.PublicationTimestamp, " | ")
+	}
+
+	printers := []func(o *fabrikant.Order){}
+	if *withUID {
+		printers = append(printers, printUid)
+	}
+	if *withType {
+		printers = append(printers, printType)
+	}
+	if *withTitle {
+		printers = append(printers, printTitle)
+	}
+	if *withTimestamp {
+		printers = append(printers, printTimestamp)
+	}
+
+	if len(printers) == 0 {
+		return
+	}
+
 	for _, o := range orders {
-		fmt.Println(o.UID, o.PublicationTimestamp)
+		for _, p := range printers {
+			p(o)
+		}
+		fmt.Println()
 	}
 }
